28b8746b253c46bd9bc3faba17fe5bcab2d848f4
Piuma Nera <moh.messafa@gmail.com>
2024-03-16 15:59:09 +0100

    contact without mail

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import "./app.scss"
+import "./app.scss";
+import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
@@ -6,17 +7,27 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 
 const App = () => {
-  return <div>
-    <section id="Homepage">
-        <Navbar/>
-        <Hero/>
-    </section>
-    <section id="Services"><Parallax type="services"/></section>
-    <section><Services/></section>
-    <section id="Portfolio"><Parallax type="portfol"/></section>
-    <Portfolio/>
-    <section id="Contact">Contact</section>
-  </div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfol" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+    </div>
+  );
 };
 
 export default App;
